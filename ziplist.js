b85bf2857e3b7cfb3894e9108ea4b454babd689b
Gavin Peng <gavin4@hawaii.edu>
2022-02-06 14:27:08 -1000

    second commit

--- ziplist.js
+++ ziplist.js
@@ -1,19 +1,1 @@
-let arr1 = ['a', 'b', 'c'];
-let arr2 = [1, 2, 3];
-function zipList(arr1, arr2) {
-  let returnArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-    returnArr.push(arr1[i]);
-    returnArr.push(arr2[i]);
-  }
-  return returnArr;
-}
-
-console.log(zipList(arr1, arr2));
-
-function zipListTheSimpleWay(arr1, arr2) {
-  let zip = _.zip(arr1, arr2);
-  return _.flatten(zip);
-}
-
-console.log(zipListTheSimpleWay(arr1, arr2));
+let arr1 = ['a', 'b', 'c'];let arr2 = [1, 2, 3];function zipList(arr1, arr2) {  let returnArr = [];  for (let i = 0; i < arr1.length; i++) {    returnArr.push(arr1[i]);    returnArr.push(arr2[i]);  }  return returnArr;}console.log(zipList(arr1, arr2));function zipListTheSimpleWay(arr1, arr2) {  let zip = _.zip(arr1, arr2);  return _.flatten(zip);}console.log(zipListTheSimpleWay(arr1, arr2));
